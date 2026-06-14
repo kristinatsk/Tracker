fec362d5e7c8965b0c8a231574a5712a0dbff8c5
kristinatsk <kristinatsk@gmail.com>
2026-06-14 11:49:21 +0300

    fix: add logger to Analytics

--- Helpers/AnalyticsService.swift
+++ Helpers/AnalyticsService.swift
@@ -1,16 +1,20 @@
 import Foundation
 import AppMetricaCore
+import os
 
 struct AnalyticsService {
+    private static let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "Tracker", category: "Analytics")
+    private static let apiKey = "9b3ab57e-2d03-40b9-ac76-371e89cd2ca3"
+    
     static func activate() {
-        guard let configuration = AppMetricaConfiguration(apiKey: "9b3ab57e-2d03-40b9-ac76-371e89cd2ca3") else { return }
+        guard let configuration = AppMetricaConfiguration(apiKey: apiKey) else { return }
         AppMetrica.activate(with: configuration)
     }
     
     static func report(event: String, params: [AnyHashable : Any]) {
-        print("Analytics: event sent \(event) with params \(params)")
+        logger.info("Analytics: event sent \(event) with params \(params)")
         AppMetrica.reportEvent(name: event, parameters: params, onFailure: { error in
-            print("REPORT ERROR: %@", error.localizedDescription)
+            logger.error("REPORT ERROR: \(error.localizedDescription)")
         })
     }
 }
